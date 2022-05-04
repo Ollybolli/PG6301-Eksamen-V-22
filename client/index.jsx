de226dc7727d0a6e86fed71d5e1ca5203b235142
ollybolli <oliver_smil@hotmail.com>
2022-05-04 14:52:58 +0200

    ListNews on the client side
    *useLoader
    *fetchJSON

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
@@ -12,11 +12,64 @@ function FrontPage() {
     </div>;
 }
 
+function useLoading(loadingFunction) {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+    const [data, setData] = useState();
+
+    async function load() {
+        try {
+            setLoading(true);
+            setData(await loadingFunction());
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        load();
+    }, []);
+
+    return {
+        loading, error, data };
+}
+
+
+async function fetchJSON(url) {
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`Failed to load ${res.status}: ${res.statusText}`);
+    }
+    return await res.json();
+}
+
 function ListNews() {
+    const { loading, error, data } = useLoading(async () => fetchJSON("/api/news")
+);
+
+if (loading) {
+    return <div>Loading...</div>;
+}
+if(error) {
+    return <div>
+        <h1>Error</h1>
+        <div>{error.toString()}</div>
+    </div>
+}
     return <div>
         <h1>News in the database</h1>
+
+        <ul>
+            {data.map(news =>
+                <li key={news.title}>{news.title}</li>
+            )}
+        </ul>
     </div>;
 }
+
+
 
 function AddNewNews() {
     return <form>
